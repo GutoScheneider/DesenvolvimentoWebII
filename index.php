88dd43de294c3e2d14fadf0a671f705b06c561d2
João Augusto Scheneider <41909735+GutoScheneider@users.noreply.github.com>
2021-03-10 19:45:36 -0300

    Criado novo repositorio

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require('autoload.php');
+
+$metaCharset = new Meta("UTF-8");
+$metaHttEquiv = new Meta(null, null, "X-UA-Compatible", "IE=edge");
+$metaName = new Meta(null, "viewport", null, "width=device-width, initial-scale=1.0");
+
+$title = new Title("Minha Página");
+
+$head = new Head();
+$head->addElement($metaCharset);
+$head->addElement($metaHttEquiv);
+$head->addElement($metaName);
+$head->addElement($title);
+
+$body = new Body("body");
+
+$html = new Html("pt-br", $head, $body);
+
+echo $html;
